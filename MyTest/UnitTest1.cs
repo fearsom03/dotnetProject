e0690bc4304d9a59c593d6678e3e108f3effc049
Kuanysh Salyk <kazakhkuka@gmail.com>
2019-11-16 06:22:18 +0600

    * UnitTest1.cs: Finish Tasks
    * UnitTest2.cs: Final tests

--- MyTest/UnitTest1.cs
+++ MyTest/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Milestone_1.Abstractions;
 using Milestone_1.models;
 using Milestone_1.Services;
@@ -10,7 +12,7 @@ namespace MyTest
     public class UnitTest1
     {
         [Fact]
-        public async void Test1()
+        public async Task CreateUserTest()
         {
             var fakeRepository = Mock.Of<IAllRepo>();
             var movieService = new UserDataService(fakeRepository);
@@ -19,5 +21,75 @@ namespace MyTest
             await movieService.AddUser(user);
 
         }
+        [Fact]
+        public async Task UpdateUserTest()
+        {
+            var fake = Mock.Of<IAllRepo>();
+            var roleService = new UserDataService(fake);
+
+            var role = new User() { login = "update-admin" };
+            await roleService.UpdateUser(role.id);
+
+        }
+
+        [Fact]
+        public async Task DeleteUserTest()
+        {
+            var fake = Mock.Of<IAllRepo>();
+            var roleService = new UserDataService(fake);
+
+            var role = new User() { login = "update-admin" };
+            await roleService.DeleteUser(role);
+
+        }
+
+        [Fact]
+        public async Task DetailUserTest()
+        {
+            var fake = Mock.Of<IAllRepo>();
+            var roleService = new UserDataService(fake);
+
+            var role = new User() { login = "update-admin" , password = "password" };
+            await roleService.DetailsUsers(role.id);
+        }
+
+        [Fact]
+        public async Task GetAllUser()
+        {
+
+
+            var surveys = new List<User>
+            {
+                new User() { login="login", password="pasword" },
+                new User() { login="login11",password="pasword11"}
+            };
+
+            var fake = new Mock<IAllRepo>();
+            fake.Setup(x => x.GetUsers()).ReturnsAsync(surveys);
+
+
+            var service = new UserDataService(fake.Object);
+            var users = await service.GetUsers();
+
+
+            Assert.Collection(users, user
+                =>
+            {
+                Assert.Equal("login", user.login);
+                Assert.Equal("pasword", user.password);
+            },
+            user =>
+            {
+                Assert.Equal("login11", user.login);
+                Assert.Equal("pasword11", user.password);
+            });
+
+
+            
+        }
+
+
+
+
     }
 }
---
+++ MyTest/UnitTest2.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Milestone_1.Abstractions;
+using Milestone_1.models;
+using Milestone_1.Services;
+using Moq;
+using Xunit;
+
+namespace MyTest
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public async Task CreateUserDataTest()
+        {
+            var fakeRepository = Mock.Of<IAllRepo>();
+            var movieService = new UserDataService(fakeRepository);
+
+            var user = new UserData() { name="kuka",surname="Altyn",birthDate=1998,city="Ala",country="USA",gender="MALE"};
+            await movieService.AddUserData(user);
+
+        }
+        [Fact]
+        public async Task UpdateUserDataTest()
+        {
+            var fake = Mock.Of<IAllRepo>();
+            var roleService = new UserDataService(fake);
+
+            var role = new UserData() { name = "update-admin" };
+            await roleService.UpdateUserData(role);
+
+        }
+
+        [Fact]
+        public async Task DeleteUserDataTest()
+        {
+            var fake = Mock.Of<IAllRepo>();
+            var roleService = new UserDataService(fake);
+
+            var role = new UserData() { name = "update-admin" };
+            await roleService.DeleteUserData(role);
+
+        }
+
+        [Fact]
+        public async Task DetailUserDataTest()
+        {
+            var fake = Mock.Of<IAllRepo>();
+            var roleService = new UserDataService(fake);
+
+            var user = new UserData() { name = "kuka", surname = "Altyn", birthDate = 1998, city = "Ala", country = "USA", gender = "MALE" };
+            await roleService.DetailsUsers(user.UserDataId);
+        }
+
+        [Fact]
+        public async Task GetAllUserData()
+        {
+
+
+            var surveys = new List<UserData>
+            {
+                new UserData() { name = "kuka", surname = "Altyn", birthDate = 1998, city = "Ala", country = "USA", gender = "MALE" },
+                new UserData() { name = "kuka1", surname = "Altyn1", birthDate = 1999, city = "Ala", country = "USA", gender = "MALE" }
+        };
+
+            var fake = new Mock<IAllRepo>();
+            fake.Setup(x => x.GetUserDatas()).ReturnsAsync(surveys);
+
+
+            var service = new UserDataService(fake.Object);
+            var users = await service.GetUserDatas();
+
+
+            Assert.Collection(users, UserData
+                =>
+            {
+                Assert.Equal("kuka", UserData.name);
+                Assert.Equal("Altyn", UserData.surname);
+                Assert.Equal(1998, UserData.birthDate);
+                Assert.Equal("Ala", UserData.city);
+                Assert.Equal("USA", UserData.country);
+                Assert.Equal("MALE", UserData.gender);
+
+            },
+            user =>
+            {
+                Assert.Equal("kuka1", user.name);
+                Assert.Equal("Altyn1", user.surname);
+                Assert.Equal(1999, user.birthDate);
+                Assert.Equal("Ala", user.city);
+                Assert.Equal("USA", user.country);
+                Assert.Equal("MALE", user.gender);
+            });
+
+
+
+        }
+    }
+}
